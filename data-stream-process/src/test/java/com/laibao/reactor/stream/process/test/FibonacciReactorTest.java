b2e7f49a525450cedf9d6473c949939f81fb92f9
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-25 14:33:38 +0800

    update pom.xml file info

--- data-stream-process/src/test/java/com/laibao/reactor/stream/process/test/FibonacciReactorTest.java
+++ data-stream-process/src/test/java/com/laibao/reactor/stream/process/test/FibonacciReactorTest.java
@@ -1,13 +1,27 @@
 package com.laibao.reactor.stream.process.test;
 
 import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuples;
 
 public class FibonacciReactorTest {
 
     @Test
     public void testFibonacciNumber() {
 
-       // Flux<Long> fibonacciGenerator =
+        Flux<Long> fibonacciGenerator = Flux.generate(() -> Tuples.<Long,Long>of(0L, 1L),
+                                                            (state, sink) -> {
+                                                                if (state.getT1() < 0) {
+                                                                    sink.complete();
+                                                                } else {
+                                                                    sink.next(state.getT1());
+                                                                }
+                                                            return Tuples.of(state.getT2(), state.getT1() + state.getT2());
+                                        });
+
+        fibonacciGenerator.subscribe(t -> {
+                                    System.out.println(t);
+        });
     }
 
 }
