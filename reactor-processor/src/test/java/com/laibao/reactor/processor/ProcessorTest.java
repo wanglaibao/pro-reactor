c73ad6a134b33ad9cf522a6da9c0769e7ffd3ef7
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-26 14:54:50 +0800

    update pom.xml file info

--- reactor-processor/src/test/java/com/laibao/reactor/processor/ProcessorTest.java
+++ reactor-processor/src/test/java/com/laibao/reactor/processor/ProcessorTest.java
@@ -13,4 +13,37 @@ public class ProcessorTest {
         data.onNext(11L);                                                // 发布事件
         data.onNext(12L);                                                // 发布事件
     }
+
+
+    @Test
+    public void testDirectProcessorFinishedEvent() {
+        DirectProcessor<Long> data = DirectProcessor.create();
+        data.subscribe(t -> System.out.println(t),
+                       e -> e.printStackTrace(),
+                       () -> System.out.println("Finished 1"));
+        data.onNext(10L);
+        data.onComplete();
+        data.onNext(15L);
+        data.onNext(13L);
+        data.onNext(14L);
+
+        data.subscribe(t -> System.out.println(t),
+                       e -> e.printStackTrace(),
+                       () -> System.out.println("Finished 2"));
+
+        data.onNext(12L);
+    }
+
+    @Test
+    public void testDirectProcessorErrorEvent() {
+        DirectProcessor<Long> data = DirectProcessor.create();
+
+        data.subscribe(t -> System.out.println(t),
+                        e -> e.printStackTrace(),
+                        () -> System.out.println("Finished"),
+                        s -> s.request(1));
+        data.onNext(10L);
+        data.onNext(11L);
+        data.onNext(12L);
+    }
 }
