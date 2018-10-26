ae141331effa0ef0872832357efb183b759f2c3d
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-27 07:48:53 +0800

    update pom.xml file info

---
+++ spring-reactor/src/main/java/com/laibao/spring/reactor/controller/FibonacciController.java
@@ -0,0 +1,28 @@
+package com.laibao.spring.reactor.controller;
+
+import org.reactivestreams.Publisher;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuples;
+
+@RestController
+public class FibonacciController {
+
+    @GetMapping("/fibonacci")
+    public Publisher<Long> fibonacciSeries() {
+
+        Flux<Long> fibonacciGenerator = Flux.generate(() -> Tuples.<Long, Long>of(0L, 1L),
+                                                            (state, sink) -> {
+                                                                        if (state.getT1() < 0) {
+                                                                            sink.complete();
+                                                                        } else {
+                                                                            sink.next(state.getT1());
+                                                                        }
+                                    return Tuples.of(state.getT2(), state.getT1() + state.getT2());
+        });
+
+        return fibonacciGenerator.take(10);
+
+    }
+}
