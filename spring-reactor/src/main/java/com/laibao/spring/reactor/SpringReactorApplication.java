d3869a3b5e7f840f56eb7611c6d7e21f86edb72f
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-27 20:58:23 +0800

    update pom.xml file info

--- spring-reactor/src/main/java/com/laibao/spring/reactor/SpringReactorApplication.java
+++ spring-reactor/src/main/java/com/laibao/spring/reactor/SpringReactorApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
-@EnableWebFlux
 @SpringBootApplication
 public class SpringReactorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactorApplication.class, args);
 	}
