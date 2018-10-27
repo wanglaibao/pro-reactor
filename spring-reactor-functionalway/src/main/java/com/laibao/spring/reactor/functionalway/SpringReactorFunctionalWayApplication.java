47e61490a096bf0ca93dc3c932572b35f50a8d71
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-27 16:31:13 +0800

    update pom.xml file info

---
+++ spring-reactor-functionalway/src/main/java/com/laibao/spring/reactor/functionalway/SpringReactorFunctionalWayApplication.java
@@ -0,0 +1,14 @@
+package com.laibao.spring.reactor.functionalway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableWebFlux
+@SpringBootApplication
+public class SpringReactorFunctionalWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactorFunctionalWayApplication.class, args);
+	}
+}
