8d0f3835802753a5673d1a8b66c23cc2618c32ed
wanglaibao2010 <wanglaibao2010@gmail.com>
2018-10-28 17:10:10 +0800

    update pom.xml file info

---
+++ webflux-thymeleaf/src/main/java/com/laibao/spring/webflux/thymeleaf/SpringWebFluxThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.laibao.spring.webflux.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFluxThymeleafApplication.class, args);
+    }
+}
---
+++ webflux-thymeleaf/src/main/java/com/laibao/spring/webflux/thymeleaf/configuration/WebFluxConfiguration.java
@@ -0,0 +1,42 @@
+package com.laibao.spring.webflux.thymeleaf.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.ViewResolverRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.thymeleaf.spring5.ISpringWebFluxTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.spring5.view.reactive.ThymeleafReactiveViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@EnableWebFlux
+@Configuration
+public class WebFluxConfiguration implements WebFluxConfigurer {
+
+    //SpringWebFluxTemplateEngine
+
+    private final ISpringWebFluxTemplateEngine templateEngine;
+
+    public WebFluxConfiguration(ISpringWebFluxTemplateEngine iSpringWebFluxTemplateEngine) {
+        this.templateEngine = iSpringWebFluxTemplateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(thymeleafViewResolver());
+    }
+
+    @Bean
+    public ThymeleafReactiveViewResolver thymeleafViewResolver() {
+        final ThymeleafReactiveViewResolver viewResolver = new ThymeleafReactiveViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
+
+
+}
