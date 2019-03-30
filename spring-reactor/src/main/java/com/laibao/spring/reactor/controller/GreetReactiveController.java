85c1f61053ef2516093cd3a138791bae91cc74a2
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-30 21:59:25 +0800

    update pom.xml file info

--- spring-reactor/src/main/java/com/laibao/spring/reactor/controller/GreetReactiveController.java
+++ spring-reactor/src/main/java/com/laibao/spring/reactor/controller/GreetReactiveController.java
@@ -2,6 +2,7 @@ package com.laibao.spring.reactor.controller;
 
 import com.laibao.spring.reactor.domain.Greeting;
 import org.reactivestreams.Publisher;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -11,7 +12,7 @@ import reactor.core.publisher.Flux;
 public class GreetReactiveController {
 
     //以流的形式输出
-    @GetMapping(value = "/greetings",produces = "text/event-stream")
+    @GetMapping(value = "/greetings",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Publisher<Greeting> greetingPublisher() {
         Flux<Greeting> greetingFlux = Flux.<Greeting>generate(synchronousSink -> synchronousSink.next(new Greeting("Hello")))
                                             .take(100);
