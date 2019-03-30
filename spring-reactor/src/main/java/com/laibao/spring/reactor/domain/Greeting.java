43e2390361706243d0c1758d4619b29d8dc044b8
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-30 14:24:52 +0800

    update pom.xml file info

---
+++ spring-reactor/src/main/java/com/laibao/spring/reactor/domain/Greeting.java
@@ -0,0 +1,16 @@
+package com.laibao.spring.reactor.domain;
+
+public class Greeting {
+    private String msg;
+
+    public Greeting() {
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
