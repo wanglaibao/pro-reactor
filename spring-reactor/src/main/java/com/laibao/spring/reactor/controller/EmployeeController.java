6623a889fce9c1b74f0b9bc78de6d25ea7251f08
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-30 14:05:09 +0800

    update pom.xml file info

--- spring-reactor/src/main/java/com/laibao/spring/reactor/controller/EmployeeController.java
+++ spring-reactor/src/main/java/com/laibao/spring/reactor/controller/EmployeeController.java
@@ -3,31 +3,46 @@ package com.laibao.spring.reactor.controller;
 import com.laibao.spring.reactor.domain.Department;
 import com.laibao.spring.reactor.domain.Employee;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
 
 @RestController
 public class EmployeeController {
 
 
     @GetMapping("/contact/{deptId}/employee/{empId}")
-    public Employee findEmployee(@PathVariable long deptId, @PathVariable long empId) {
+    public Mono<Employee> findEmployee(@PathVariable long deptId, @PathVariable long empId) {
         //TODO
         // Find the employee.
         return null;
     }
 
-    @PostMapping("/department")
-    public void createDept(@RequestBody Mono<Department> dept) {
-        //TODO
-        // Add new department
-    }
 
     @GetMapping("/contact/employee")
-    public Employee findEmployeeByParam(@RequestParam("deptId")long deptId, @RequestParam("empId") long empId) {
+    public Mono<Employee> findEmployeeByParam(@RequestParam("deptId")long deptId, @RequestParam("empId") long empId) {
         //TODO
         // Find the employee.
+        return null;
+    }
+
+
+    @GetMapping("/fibonacci")
+    public Flux<Long> fibonacci(@RequestHeader("Accept-Encoding") String encoding) {
+        // TODO
+        return null;
+    }
+
+
+    @PostMapping("/department")
+    public Mono<Void> createDept(@RequestBody Mono<Department> dept) {
+        //TODO
+        // Add new department
+        return null;
+    }
+
+    @ExceptionHandler
+    public String handleError(RuntimeException ex) {
         return null;
     }
 }
