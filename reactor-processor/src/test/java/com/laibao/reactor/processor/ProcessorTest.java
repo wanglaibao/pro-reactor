9e85f5a6302ad9395d516a644afc0cbfc9a92087
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-30 08:52:05 +0800

    update pom.xml file info

--- reactor-processor/src/test/java/com/laibao/reactor/processor/ProcessorTest.java
+++ reactor-processor/src/test/java/com/laibao/reactor/processor/ProcessorTest.java
@@ -7,43 +7,55 @@ public class ProcessorTest {
 
     @Test
     public void testDirectProcessor() {
-        DirectProcessor<Long> data = DirectProcessor.create();
-        data.take(3).subscribe(number -> System.out.println(number));    //订阅事件
-        data.onNext(10L);                                                // 发布事件
-        data.onNext(11L);                                                // 发布事件
-        data.onNext(12L);                                                // 发布事件
+        DirectProcessor<Long> longDirectProcessor = DirectProcessor.create();
+        longDirectProcessor.take(3).subscribe(number -> System.out.println(number));    //订阅事件
+        longDirectProcessor.onNext(10L);                                                // 发布事件
+        longDirectProcessor.onNext(11L);                                                // 发布事件
+        longDirectProcessor.onNext(12L);                                                // 发布事件
+        longDirectProcessor.onNext(13L);
     }
 
 
     @Test
     public void testDirectProcessorFinishedEvent() {
-        DirectProcessor<Long> data = DirectProcessor.create();
-        data.subscribe(t -> System.out.println(t),
+        DirectProcessor<Long> directProcessor = DirectProcessor.create();
+        directProcessor.subscribe(t -> System.out.println(t),
                        e -> e.printStackTrace(),
                        () -> System.out.println("Finished 1"));
-        data.onNext(10L);
-        data.onComplete();
-        data.onNext(15L);
-        data.onNext(13L);
-        data.onNext(14L);
-
-        data.subscribe(t -> System.out.println(t),
+        directProcessor.onNext(10L);
+        directProcessor.onNext(11L);
+        directProcessor.onNext(12L);
+        directProcessor.onNext(13L);
+        directProcessor.onNext(14L);
+        directProcessor.onNext(15L);
+        directProcessor.onComplete();
+        directProcessor.onNext(120L);
+        directProcessor.onNext(121L);
+        directProcessor.onNext(122L);
+        directProcessor.subscribe(t -> System.out.println(t),
                        e -> e.printStackTrace(),
                        () -> System.out.println("Finished 2"));
-
-        data.onNext(12L);
+        directProcessor.onNext(123L);
+        directProcessor.onNext(124L);
+        directProcessor.onNext(125L);
     }
 
     @Test
     public void testDirectProcessorErrorEvent() {
-        DirectProcessor<Long> data = DirectProcessor.create();
-
-        data.subscribe(t -> System.out.println(t),
+        DirectProcessor<Long> directProcessor = DirectProcessor.create();
+        directProcessor.subscribe(t -> System.out.println(t),
                         e -> e.printStackTrace(),
-                        () -> System.out.println("Finished"),
-                        s -> s.request(1));
-        data.onNext(10L);
-        data.onNext(11L);
-        data.onNext(12L);
+                        () -> System.out.println("Finished 111111"),
+                        s -> s.request(5));
+        directProcessor.onNext(10L);
+        directProcessor.onNext(11L);
+        directProcessor.onNext(12L);
+        directProcessor.onNext(13L);
+        directProcessor.onNext(14L);
+        directProcessor.onNext(15L);
+        directProcessor.onComplete();
+        directProcessor.subscribe(t -> System.out.println(t),
+                e -> e.printStackTrace(),
+                () -> System.out.println("Finished 22222"));
     }
 }
